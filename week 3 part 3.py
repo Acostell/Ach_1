3515756165b874d5a958eb96b3adfdd4678274ef
Aaron <Aaron168684@yahoo.ca>
2021-09-20 11:45:31 -0400

    final

---
+++ week 3 part 3.py
@@ -0,0 +1,6 @@
+print("Enter a number: ")
+x= input()
+sqrt= float(x)**.5
+print ("The hexadecimal version is %x " % int(x))
+print ("The octadeciaml version is  %o " %int(x))
+print ("The square root is " + str(sqrt))
