5a14cd2f2cfdebae605bd9ca0b9808afc6e3c416
Aaron <Aaron168684@yahoo.ca>
2021-09-20 10:23:43 -0400

    test 1

---
+++ week 3.py
@@ -0,0 +1,7 @@
+PI=3.14
+print ("Enter circle radius:")
+radius= input()
+area= PI* float(radius)**2
+print ("The area is " + str(area))
+circumference= 2*PI*float(radius)
+print ("The circumference is " + str(circumference)) 
