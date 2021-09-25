57317966d2406589fe7bb7330a432930ecfe10e3
Aaron <Aaron168684@yahoo.ca>
2021-09-25 17:01:13 -0400

    part 1 done

--- Assignment #1.py
+++ Assignment #1.py
@@ -1,1 +1,24 @@
 #Assigntment 1, Make a store and show the commit and push after.
+
+print ("Welcome to Abby's Merchandizing!")
+print ("Please select a color of shirt you would like!")
+print (" the color options are: Red, Blue, White and Black!")
+
+color = input()
+
+if color == "Red":
+    print("You have chosen a red shirt!")
+
+elif color == "Blue":
+    print ("you have chosen Blue!")     
+
+elif color == "White":
+    print ("you have chosen White!")
+
+elif color == "Black":
+    print ("You have chosen Black")
+
+else :
+    print ("Please select a valid color from the options!")
+
+
