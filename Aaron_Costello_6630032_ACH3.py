3e00b0e38dc7516b16e2cefd70c80afcfb2f6164
Aaron <Aaron168684@yahoo.ca>
2021-10-04 11:06:06 -0400

    final

---
+++ Aaron_Costello_6630032_ACH3.py
@@ -0,0 +1,11 @@
+x = int (input("Input a whole number:"))
+sum = 0
+
+while x > 0:
+    if x % 2 ==0:
+        sum += x**2
+        x = x-1
+    else:
+        x=x-1
+    if x == 0:
+        print(sum)
