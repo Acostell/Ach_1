30004e0a0ba5921f76b7b709c12c6372c0c7babe
Aaron <Aaron168684@yahoo.ca>
2021-10-23 21:21:27 -0400

    Shirts updated and finished

--- Assignment #1.py
+++ Assignment #1.py
@@ -44,11 +44,65 @@ Shirt_quantity = int(input("How many shirts would you like?\n" "They are $9.99 e
 print ("You have selected %s shirts in total!"% (str(Shirt_quantity)))
 
 
-After_tax_cost = Shirt_cost * 1.13
+After_tax_cost = (Shirt_cost * Shirt_quantity) * 1.13
 
-print ("Your cost before tax is $%.2f!" %(Shirt_cost))
+if Shirt_quantity >=3:
+    quantity_discount = True
+else:
+    quantity_discount = False
 
-print ("Your cost after tax is $%.2f!" %(After_tax_cost)) 
+print(" Are you a senior citizen or student?\n If yes to either please enter Y or yes, if no enter N or no ")
+while True:
+    senior_discount = str(input())
+    if senior_discount == ("Y") or senior_discount == ("yes"):
+        s_discount= True
+        print("You qualify for a %10 discount! Your largest discount will be applied at checkout! ")
+        break
+    elif senior_discount == ("N") or senior_discount == ("no"):
+        s_discount = False
+        print("Sorry you don't qualify for the discount!")
+        break
+    else:
+        print("Please put in a valid response.")
 
-print ("Please procceed to payment and have a nice day!")
+if quantity_discount == True:
+    print ("Your cost before tax is $%.2f!" %(Shirt_cost * Shirt_quantity))
+    print ("Your cost after tax is $%.2f!" %(After_tax_cost)) 
+    print("Your cost after discount is:$ %.2f" %(After_tax_cost*.85))
+    print ("This is with a 15% 'discount for quantity bought! ")
+
+elif s_discount == True:
+    print ("Your cost before tax is $%.2f!" %(Shirt_cost * Shirt_quantity))
+    print ("Your cost after tax is $%.2f!" %(After_tax_cost))
+    print("Your cost after discount is:$ %.2f" %(After_tax_cost*.9))
+    print ("This is with a %10 discount")
+
+elif quantity_discount == True and s_discount == True:
+    print ("Your cost before tax is $%.2f!" %(Shirt_cost * Shirt_quantity))
+    print ("Your cost after tax is $%.2f!" %(After_tax_cost))
+    print("Your cost after discount is:$ %.2f" %(After_tax_cost *.85))
+    print ("Sorry only the largest discount is applied")
+
+else:
+    print ("Your cost before tax is $%.2f!" %(Shirt_cost * Shirt_quantity))
+    print ("Your cost after tax is $%.2f!" %(After_tax_cost))
+    print("Your cost after discount is:$ %.2f" %(After_tax_cost))
+    print ("No discounts were applied.")
+
+
+print(" Would you also like to purchase pants?")
+while True:
+
+    more = str(input("Yes or No?"))
+    if more == "Yes":
+        print("Great! Please continue to follow the prompts!")
+        break
+
+    elif more == "No":
+        print("Please proceed to payment, Have a great day!")
+        print("You're total is:%.2f" %(After_tax_cost))
+        break
+    else:
+        print(" Please enter Yes or No")
+
 
