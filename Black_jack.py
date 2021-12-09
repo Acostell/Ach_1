cc558a42a367a3ca8f5ad67ab0a0048e0b8e39b3
Aaron <Aaron168684@yahoo.ca>
2021-12-09 15:43:23 -0500

    working! need to finish dealer

--- Black_jack.py
+++ Black_jack.py
@@ -1,23 +1,35 @@
 #Black jack
 
 import random
-    
+import os
+
 class Card:
     def __init__(self,suit,name,card_value):
          #need to identify/create cards for game
         self.suit = suit
         self.name = name
         self.card_value = card_value
+    
+    #this took me 4 hours to figure out, otherwise name prints hexcode
     def __repr__(self):
         return repr(self.name)
     
 
 def print_card(names):
     for name in names:
-            print(name,'of',suit)
-    
+            print(name,'of',name.suit)
+
         
-    
+suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+names = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+card_value = {"A":11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}     
+
+
+#make a deck
+deck = []
+for suit in suits:
+    for name in names:
+        deck.append(Card(suit,name,card_value[name]))    
     
 
         
@@ -39,13 +51,13 @@ def Blackjack(deck):
     while len(player_hand) <2:
 
         #deal a card
-        dealt_card = random.choice(deck)
-        player_hand.append(dealt_card)
+        p_dealt_card = random.choice(deck)
+        player_hand.append(p_dealt_card)
         #then we have to remove it so it doesnt get dealt twice
-        deck.remove(dealt_card)
+        deck.remove(p_dealt_card)
 
         #tell player cards and score
-        player_hand_score += dealt_card.card_value
+        player_hand_score += p_dealt_card.card_value
 
         print("Your hand is: ")
         print_card(player_hand)
@@ -64,63 +76,54 @@ def Blackjack(deck):
 
         #print dealers cards, only print first card
         print("Dealers hand is: \n")
-        #print_card(dealer_hand[0])
-        print("Dealer score is: \n",dealer_hand[0].card_value)
+        print_card(dealer_hand)
+        
 
         
-        while player_hand_score >21:
+    while player_hand_score < 21:
 
-            gamble = input("Hit or Stay?")
-            gamble
-            if gamble == "Hit":
-                player_hand.append(dealt_card)
-                player_hand_score += player_hand.card_value
-                deck.remove(dealt_card)
+        gamble = input("Hit or Stay?")
+            
+        if gamble == "Hit":
+            p_dealt_card = random.choice(deck)
+            player_hand.append(p_dealt_card)
+            player_hand_score += p_dealt_card.card_value
+            deck.remove(p_dealt_card)
+            print("Your hand is: ")
+            print_card(player_hand)
+            print("Your score is: ",(player_hand_score))    
+
                 #check for ace if over
-                if player_hand_score <21 and player_hand[0].card_value == 11:
-                    player_hand[0].card_value == 1
-                    #forgot to update hand score
-                    player_hand_score -= 10
+            if player_hand_score <21 and player_hand[0].card_value == 11:
+                player_hand[0].card_value == 1
+                #forgot to update hand score
+                player_hand_score -= 10
 
-                else:
-                    break
-
-
-
-            if gamble == "Stay":
+            else:
                 break
 
 
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        if player_hand_score == 21:
-            print("You got blackjack! You win!")
-            quit()
+        if gamble == "Stay":
+            break
 
-        if player_hand_score <21 and dealer_hand_score == 21:
-            print("Dealer has blackjack! Dealer win!")
+        else:
+            print("Hit or Stay?")
 
-suits = ["Spades","Hearts","Diamonds","Clubs"]
-names = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-card_value = {"A":11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}     
+            
 
 
-#make a deck
-deck = []
-for suit in suits:
-    for name in names:
-        deck.append(Card(suit,name,card_value[name]))
+    if player_hand_score >21:
+        print(" You bust! \n Dealer wins!")
+
+    if player_hand_score == 21:
+        print("You got blackjack! You win!")
+        quit()
+
+    if player_hand_score <21 and dealer_hand_score == 21:
+        print("Dealer has blackjack! Dealer win!")
+
+
 
 Blackjack(deck)
 
